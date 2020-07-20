59cf53eb46266dd55313987f0ff955bc35399939
Masayoshi Mizutani <mizutani@cookpad.com>
2020-07-20 17:19:21 +0900

    Use tracker.js instead of tracker.ts because compile error by NodejsFunction

--- lib/uguisu-stack.ts
+++ lib/uguisu-stack.ts
@@ -38,7 +38,7 @@ export class UguisuStack extends cdk.Stack {
     });
 
     this.tracker = new NodejsFunction(this, "tracker", {
-      entry: path.join(__dirname, "lambda/tracker.ts"),
+      entry: path.join(__dirname, "lambda/tracker.js"),
       handler: "main",
       timeout: cdk.Duration.seconds(300),
       role: role,
