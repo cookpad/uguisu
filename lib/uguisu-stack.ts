5e1e3d5654a11ad72e6569728efbdd092399f700
Masayoshi Mizutani <mizutani@cookpad.com>
2020-07-23 17:18:02 +0900

    Allow to create IAM role by S3 bucket in uguisu-stack.ts

--- lib/uguisu-stack.ts
+++ lib/uguisu-stack.ts
@@ -3,14 +3,19 @@ import * as lambda from "@aws-cdk/aws-lambda";
 import * as sns from "@aws-cdk/aws-sns";
 import * as sqs from "@aws-cdk/aws-sqs";
 import * as iam from "@aws-cdk/aws-iam";
+import * as s3 from "@aws-cdk/aws-s3";
 import { SqsEventSource } from "@aws-cdk/aws-lambda-event-sources";
 import { SqsSubscription } from "@aws-cdk/aws-sns-subscriptions";
-import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "@aws-cdk/aws-lambda-nodejs";
 import * as path from "path";
 
 export interface Arguments {
   snsTopicARN: string;
-  lambdaRoleARN: string;
+  lambdaRoleARN?: string;
+  s3BucketName?: string;
   slackWebhookURL: string;
   sentryDSN?: string;
   disableRules?: string;
@@ -28,6 +33,12 @@ export class UguisuStack extends cdk.Stack {
   ) {
     super(scope, id, props);
 
+    if (!args.lambdaRoleARN && !args.s3BucketName) {
+      throw new Error(
+        "Either one of lambdaRoleARN and s3BucketName is required"
+      );
+    }
+
     this.s3EventQueue = new sqs.Queue(this, "s3EventQueue", {
       visibilityTimeout: cdk.Duration.seconds(300),
     });
@@ -35,22 +46,33 @@ export class UguisuStack extends cdk.Stack {
     const topic = sns.Topic.fromTopicArn(this, "s3Event", args.snsTopicARN);
     topic.addSubscription(new SqsSubscription(this.s3EventQueue));
 
-    const role = iam.Role.fromRoleArn(this, "Lambda", args.lambdaRoleARN, {
-      mutable: false,
-    });
+    const role = args.lambdaRoleARN
+      ? iam.Role.fromRoleArn(this, "Lambda", args.lambdaRoleARN, {
+          mutable: false,
+        })
+      : undefined;
 
-    this.tracker = new NodejsFunction(this, "tracker", {
+    const prop: NodejsFunctionProps = {
       entry: path.join(__dirname, "lambda/tracker.js"),
       handler: "main",
       timeout: cdk.Duration.seconds(300),
-      role: role,
       memorySize: 1024,
+      role: role,
       events: [new SqsEventSource(this.s3EventQueue, { batchSize: 1 })],
       environment: {
         SLACK_WEBHOOK_RUL: args.slackWebhookURL,
         SENTRY_DSN: args.sentryDSN || "",
         DISABLE_RULES: args.disableRules || "",
       },
-    });
+    };
+
+    this.tracker = new NodejsFunction(this, "tracker", prop);
+
+    if (!role && args.s3BucketName) {
+      const bucket = s3.Bucket.fromBucketAttributes(this, "ImportedBucket", {
+        bucketArn: "arn:aws:s3:::" + args.s3BucketName,
+      });
+      bucket.grantRead(this.tracker);
+    }
   }
 }
--- test/uguisu.test.ts
+++ test/uguisu.test.ts
@@ -1,6 +1,10 @@
 import "@aws-cdk/assert/jest";
 import * as cdk from "@aws-cdk/core";
 import * as Uguisu from "../lib/uguisu-stack";
+import {
+  arrayWith,
+  objectLike,
+} from "@aws-cdk/assert/lib/assertions/have-resource";
 
 test("Build Stack", () => {
   const app = new cdk.App();
@@ -28,4 +32,60 @@ test("Build Stack", () => {
       },
     },
   });
+});
+
+test("Build Stack with S3 bucket", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Uguisu.UguisuStack(app, "MyTestStack", {
+    s3BucketName: "my-test-bucket",
+    slackWebhookURL: "https://hooks.slack.com/services/1234/5678/ABCDEFG",
+    snsTopicARN: "arn:aws:sns:ap-northeast-1:1234567890:s3-cloudtrail-event",
+  });
+  // THEN
+  expect(stack).toHaveResource("AWS::SQS::Queue", {
+    VisibilityTimeout: 300,
+  });
+  expect(stack).toHaveResource("AWS::SNS::Subscription", {
+    TopicArn: "arn:aws:sns:ap-northeast-1:1234567890:s3-cloudtrail-event",
+    Protocol: "sqs",
+  });
+  expect(stack).toHaveResource("AWS::Lambda::Function", {
+    Environment: {
+      Variables: {
+        SLACK_WEBHOOK_RUL: "https://hooks.slack.com/services/1234/5678/ABCDEFG",
+        SENTRY_DSN: "",
+        DISABLE_RULES: "",
+      },
+    },
+  });
+
+  expect(stack).toHaveResourceLike("AWS::IAM::Policy", {
+    PolicyDocument: {
+      Statement: arrayWith(
+        objectLike({
+          Action: ["s3:GetObject*", "s3:GetBucket*", "s3:List*"],
+          Effect: "Allow",
+          Resource: [
+            "arn:aws:s3:::my-test-bucket",
+            "arn:aws:s3:::my-test-bucket/*",
+          ],
+        })
+      ),
+    },
+  });
+});
+
+test("Error buliding stack without S3 bucket and lambda role", () => {
+  const app = new cdk.App();
+
+  const newUguisu = () => {
+    new Uguisu.UguisuStack(app, "MyTestStack", {
+      slackWebhookURL: "https://hooks.slack.com/services/1234/5678/ABCDEFG",
+      snsTopicARN: "arn:aws:sns:ap-northeast-1:1234567890:s3-cloudtrail-event",
+    });
+  };
+
+  // WHEN
+  expect(newUguisu).toThrowError();
 });
